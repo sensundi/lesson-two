30221d469ba9b9690af05d426d273b40e4e1fb95
Curtis Maloney <curtis@tinbrain.net>
2015-07-23 18:50:01 +1000

    Start normalising the models

--- timetracker/entries/admin.py
+++ timetracker/entries/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 
-admin.site.register(models.Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('project', 'description', 'start', 'stop')
+    list_filter = ('project',)
+    search_fields = ('project', 'description',)
+
+admin.site.register(models.Entry, EntryAdmin)
+admin.site.register(models.Project)
--- timetracker/entries/models.py
+++ timetracker/entries/models.py
@@ -2,10 +2,24 @@ from django.db import models
 
 from django.utils import timezone
 
+
+class Project(models.Model):
+    client = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '<{}> {}'.format(self.client, self.name)
+
+
 # Create your models here.
 class Entry(models.Model):
     start = models.DateTimeField(default=timezone.now)
     stop = models.DateTimeField(blank=True, null=True)
-    client = models.CharField(max_length=200)
-    project = models.CharField(max_length=200)
+    project = models.ForeignKey('Project')
     description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '[{} - {}] ({}) {}'.format(self.start, self.stop, self.project, self.description)
+
+    def is_finished(self):
+        return self.stop is not None
