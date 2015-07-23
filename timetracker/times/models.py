07d9ca6129152322a8d73a1fd0ff42c9ca4c8905
Curtis Maloney <curtis@tinbrain.net>
2015-07-23 15:22:33 +1000

    Add basic models

--- timetracker/times/models.py
+++ timetracker/times/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+from django.utils import timezone
+
 # Create your models here.
+class Entry(models.Model):
+    start = models.DateTimeField(default=timezone.now)
+    stop = models.DateTimeField(blank=True, null=True)
+    client = models.CharField(max_length=200)
+    project = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
