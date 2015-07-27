50fb4e17f2e5172d2968d1f849c0236832cdee38
SenSundi <sensundi@sen-Ubuntu12-64.(none)>
2015-07-27 14:54:57 +1000

    Completed exercise for lesson-two. Accomplished the following 	1. Normalized models to separate client and project data 	2. Implemented migrations to re-use the client data from 	project model instead. 	3. Added new model client to admin interface

--- timetracker/entries/migrations/0001_initial.py
+++ timetracker/entries/migrations/0001_initial.py
@@ -14,12 +14,23 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Entry',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('start', models.DateTimeField(default=django.utils.timezone.now)),
-                ('stop', models.DateTimeField(blank=True, null=True)),
-                ('client', models.CharField(max_length=200)),
-                ('project', models.CharField(max_length=200)),
+                ('stop', models.DateTimeField(null=True, blank=True)),
                 ('description', models.CharField(max_length=200)),
             ],
+        ),
+        migrations.CreateModel(
+            name='Project',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('client', models.CharField(max_length=200)),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='entry',
+            name='project',
+            field=models.ForeignKey(to='entries.Project'),
         ),
     ]
